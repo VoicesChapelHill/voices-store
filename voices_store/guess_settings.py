4fe6e0bf4a4a9e702591881c532ae69feaa6238e
Dan Poirier <dpoirier@caktusgroup.com>
2014-01-09 15:49:34 -0500

    Log hostname from guess_settings

--- voices_store/guess_settings.py
+++ voices_store/guess_settings.py
@@ -1,8 +1,13 @@
+import logging
 import socket
+
+logger = logging.getLogger(__name__)
+
 
 def guess_settings():
     # Return name of a settings module to use given the current environment
     hostname = socket.getfqdn()
+    logger.info("hostname=%s" % hostname)
     if hostname.startswith("dokku"):
         return "voices_store.settings.dokku"
     return "voices_store.settings.local"
