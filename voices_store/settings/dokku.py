d98a20868000de39e80481da78c9348f21622617
Dan Poirier <dan@poirier.us>
2014-02-23 12:50:16 -0500

    Log everything from dokku to stdout

--- voices_store/settings/dokku.py
+++ voices_store/settings/dokku.py
@@ -1,8 +1,31 @@
 from .base import *
 
 import dj_database_url
+import logging
 
 DATABASES = {'default': dj_database_url.config()}
 STRIPE_SECRET_KEY = os.environ['STRIPE_SECRET_KEY']
 STRIPE_PUBLISHABLE_KEY = os.environ['STRIPE_PUBLISHABLE_KEY']
 MEMBER_PASSWORD = os.environ['MEMBER_PASSWORD']
+
+# Log everything to stdout
+LOGGING = {
+    'version': 1,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler'
+        },
+    },
+
+    'loggers': {
+        '': {
+            'level': logging.WARN,
+            'handlers': ['console'],
+        },
+        'django': {
+            'level': logging.WARN,
+            'handlers': ['console'],
+        },
+    }
+}
